fe1cc31333c04b59731aca87694ef8d34683b4d8
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 02:40:54 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -10,4 +10,7 @@ class Doctor
     @name = name
     @@all << self
   end
-  
+
+  def new_appointment(date, patient)
+    new_appt = Appointment.new(date, patient, self)
+    new_appt.doctor = self
