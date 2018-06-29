63d394522e2130ff43931543343f4277089cd2b6
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 02:14:43 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -5,6 +5,21 @@ class Artist
     @all
   end
 
-  
+  def initialize(name)
+    @name = name
+    self.all << self
+  end
+
+  def new_song(name, genre)
+    
+  end
+
+  def songs
+
+  end
+
+  def genres
+
+  end
 
 end
