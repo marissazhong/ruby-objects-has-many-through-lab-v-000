0bad76ae2e6659ee4ede5086c0e02d53c1e51264
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 02:17:20 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+
+  def self.all
+    @all
+  end
+
+  def initialize(name)
+    @name = name
+    self.all << self
+  end
+
+  def new_song(name, genre)
+    new_song = Song.new(name)
+  end
+
+  def songs
+
+  end
+
+  def genres
+
+  end
+
+end
