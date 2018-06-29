5aea235fe80f907211d03ba85ee754d53d6f1c91
Marissa Zhong <mwz2101@columbia.edu>
2018-06-29 02:43:03 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,3 +14,12 @@ class Doctor
   def new_appointment(date, patient)
     new_appt = Appointment.new(date, patient, self)
     new_appt.doctor = self
+  end
+
+  def appointments
+    Appointment.all.select {|appt| appt.doctor == self}
+  end
+
+  def patients
+    patients = []
+    appointments.each {|appt| patients << appt.patient}
